c9291afb405ae874d227cf903c07be0800ff1ee3
muroslavko <myroslavdm@gmail.com>
2015-07-01 12:48:42 +0300

    41: Add Square class

---
+++ Test_Program/Test_Program/Square.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Program
+{
+    class Square
+    {
+        public double Circle(int r)
+        {
+            double square = 2 * Math.PI * r;
+            return square;
+        }
+    }
+}
